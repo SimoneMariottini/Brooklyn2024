5c6a6678b1462398c8157f2920b192711f5615e5
rbob14 <mattacchione.1921852@studenti.uniroma1.it>
2024-05-22 15:23:55 +0200

    fit testing

--- Macros/FitChargePoisson.C
+++ Macros/FitChargePoisson.C
@@ -5,7 +5,7 @@
 void FitChargePoisson(){
 
     TString path("/Users/robertomattacchione/Desktop/lab2/");
-    TString inname("run_160524_trgledonch15_lowlight_long.root");
+    TString inname("run_170524_trgledonch0_lowlight_long.root");
     TFile *f = new TFile(path + inname, "READ");
     f->cd();
 
@@ -16,8 +16,10 @@ void FitChargePoisson(){
     TF1 * fun = new TF1("PoisGaus", AnaTools::poisGausFun, -0.01, 0.05, 6);
 
     double par[6] = {-0.0018, 0.0005, 0.0018, 0.0008, 2, 1};
+    //double par[6] = {0.0015, 0.0005, 0.0022, 0.0005, 2, 12};
 
     fun->SetParameters(par);
+    fun->SetParLimits(2,0.0015,0.0018);
     fun->SetNpx(10000);
     
     TDirectory* dir;
@@ -31,7 +33,7 @@ void FitChargePoisson(){
         //int i = 2;
 
         for (int i = 0; i < NCHANNELS; i++)
-        {
+        { 
             auto c1 = new TCanvas(Form("Channel_%i", i), Form("Channel_%i", i), 1200, 800);
     
             c1->cd();
