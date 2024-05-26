1cc78d1e95fe888d5cf0561f1cceff2d7335d48a
almaguarino <167882682+almaguarino@users.noreply.github.com>
2024-05-26 16:21:03 +0200

    Update FitTimeGaussian.C

--- Macros/FitTimeGaussian.C
+++ Macros/FitTimeGaussian.C
@@ -13,7 +13,7 @@ void FitTimeGaussian(){
 
  
     TDirectory* dir;
-    dir = f->GetDirectory("./Hist_Time/Hist_Time_Method_1");
+    dir = f->GetDirectory("Hist_Time");
 
     //TF1 * fun = new TF1("Gaus", AnaTools::GausFun, -0.01, 0.05, 6);
     TF1 *fun = new TF1("Gaus", "[2]*TMath::Gaus(x,[0],[1])");
