9d1a6b9595035bba31458b0c0d5f285ccd536aa3
rbob14 <mattacchione.1921852@studenti.uniroma1.it>
2024-06-06 10:26:30 +0200

    save gain macro

--- Macros/SaveGain.C
+++ Macros/SaveGain.C
@@ -6,7 +6,23 @@ void SaveGain(){
     //first set the gain values for every channel
     int channel = 1;
     double gain_value = 1;
-    MyAnaTools->SetGain(channel, gain_value);
+    MyAnaTools->SetGain(0, );
+    MyAnaTools->SetGain(1, );
+    MyAnaTools->SetGain(2, );
+    MyAnaTools->SetGain(3, );
+    MyAnaTools->SetGain(4, );
+    MyAnaTools->SetGain(5, );
+    MyAnaTools->SetGain(6, );
+    MyAnaTools->SetGain(7, );
+    MyAnaTools->SetGain(8, );
+    MyAnaTools->SetGain(9, );
+    MyAnaTools->SetGain(10, );
+    MyAnaTools->SetGain(11, );
+    MyAnaTools->SetGain(12, );
+    MyAnaTools->SetGain(13, );
+    MyAnaTools->SetGain(14, );
+    MyAnaTools->SetGain(15, );
+
 
     //then save info to file
     MyAnaTools->SaveInfo("gain");
